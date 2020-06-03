b9f2ca28f6fece224037849ac8c5021da6ec1a47
uanoob <usnet@ukr.net>
2020-06-03 09:56:22 +0300

    Updated Authentication component for sign up

--- src/pages/authentication/index.jsx
+++ src/pages/authentication/index.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import useFetch from '../../hooks/useFetch';
 
-const Authentication = () => {
+const Authentication = ({ match }) => {
+  const isLogin = match.path === '/login';
+  const pageTitle = isLogin ? 'Sign In' : 'Sign Up';
+  const descriptionLink = isLogin ? '/register' : '/login';
+  const descriptionText = isLogin ? 'Need an account?' : 'Have an account?';
+  const apiUrl = isLogin ? '/users/login' : '/users';
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [{ isLoading }, doFetch] = useFetch('/users/login');
+  const [username, setUsername] = useState('');
+  const [{ isLoading }, doFetch] = useFetch(apiUrl);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const user = isLogin ? { email, password } : { email, password, username };
     doFetch({
       method: 'post',
       data: {
-        user: {
-          email,
-          password,
-        },
+        user,
       },
     });
   };
@@ -25,12 +31,23 @@ const Authentication = () => {
       <div className="container page">
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Login</h1>
+            <h1 className="text-xs-center">{pageTitle}</h1>
             <p className="text-xs-center">
-              <Link to="register">Need an account?</Link>
+              <Link to={descriptionLink}>{descriptionText}</Link>
             </p>
             <form onSubmit={handleSubmit}>
               <fieldset>
+                {!isLogin && (
+                  <fieldset className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      placeholder="Username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </fieldset>
+                )}
                 <fieldset className="form-group">
                   <input
                     type="email"
@@ -54,7 +71,7 @@ const Authentication = () => {
                   type="submit"
                   disabled={isLoading}
                 >
-                  Sign in
+                  {pageTitle}
                 </button>
               </fieldset>
             </form>
@@ -63,6 +80,12 @@ const Authentication = () => {
       </div>
     </div>
   );
+};
+
+Authentication.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Authentication;
