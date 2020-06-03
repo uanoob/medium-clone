28a1e8d78c58ed80f1aaaa8c2bd2ea93fcb18540
uanoob <usnet@ukr.net>
2020-06-03 08:28:18 +0300

    Addded Authentication component, useFetch

---
+++ src/pages/authentication/index.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+
+const Authentication = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [{ isLoading }, doFetch] = useFetch('/users/login');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    doFetch({
+      method: 'post',
+      data: {
+        user: {
+          email,
+          password,
+        },
+      },
+    });
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Login</h1>
+            <p className="text-xs-center">
+              <Link to="register">Need an account?</Link>
+            </p>
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </fieldset>
+                <button
+                  className="btn btn-lg btn-primary pull-xs-right"
+                  type="submit"
+                  disabled={isLoading}
+                >
+                  Sign in
+                </button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Authentication;
--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import GlobalFeed from './globalFeed';
 import Article from './article';
+import Authentication from './authentication';
 
 export default () => (
   <Switch>
     <Route path="/" exact component={GlobalFeed} />
+    <Route path="/login" component={Authentication} />
+    <Route path="/register" component={Authentication} />
     <Route path="/articles/:slug" component={Article} />
   </Switch>
 );
